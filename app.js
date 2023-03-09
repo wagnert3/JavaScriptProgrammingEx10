780b82cbbb3b0eb1384821450f2536309ae54c86
Wagner Santiago <66319302+wagnert3@users.noreply.github.com>
2023-03-09 12:27:23 -0400

    Ex 10 completed

--- app.js
+++ app.js
@@ -3,11 +3,49 @@ let myDiv = document.createElement('div');
 
 // set attributes of my div
 myDiv.setAttribute('id', 'newDiv');
-myDiv.setAttribute('class','box');
+myDiv.setAttribute('class','box box2');
 
 // Create text node
 let divContent = document.createTextNode('We are creating a new div!');
 
 myDiv.appendChild(divContent);
 
+//same as: myDiv.innerText = 'We are creating a new div!';
+
 document.body.appendChild(myDiv);
+
+let newParagraph = document.createElement('p');
+newParagraph.setAttribute('id', 'newParagraph');
+newParagraph.innerHTML = '<h3>This is my paragraph</h3>';
+myDiv.appendChild(newParagraph);
+
+let button = document.createElement('button');
+button.setAttribute('class', 'myButton');
+button.innerText = 'Click me';
+
+myDiv.insertBefore(button, newParagraph);
+
+let p = document.createElement('p');
+myDiv.appendChild(p);
+
+button.addEventListener('click', () => {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+
+    button.style.color = `rgb(${r},${g},${b})`;
+
+    r = Math.floor(Math.random() * 255);
+    g = Math.floor(Math.random() * 255);
+    b = Math.floor(Math.random() * 255);
+
+    myDiv.style.backgroundColor = `rgb(${r},${g},${b})`;
+
+    p.innerText = `Div bg colour is r:${r} g:${g} b:${g}`;
+
+    r = Math.floor(Math.random() * 255);
+    g = Math.floor(Math.random() * 255);
+    b = Math.floor(Math.random() * 255);
+
+    document.body.style.backgroundColor = `rgb(${r},${g},${g}`;
+});
